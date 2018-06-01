cbb91adce6f745eb4debf459896fe780c09f895e
Changmin Lim <chang.lim92@gmail.com>
2018-06-01 04:12:36 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,27 @@
 # require modules here
 
-def load_library
-  
+require 'YAML' 
+
+def load_library(file)
+  yaml = YAML.load_file(file)
+  result = {}
+  result["definition"] = {}
+  result["emoticon"] = {}
+    yaml.each do |meaning, emote|
+      result["definition"][emote[1]] = meaning 
+      result["emoticon"][emote[0]] = emote[1]
+    end 
+    result 
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file, emoticon)
+  result = load_library(file)
+  result["emoticon"].each do |key, emote|
+    if key == emoticon
+      emoticon 
+    end 
+  end 
+  "No emoticon found"
 end
 
 def get_english_meaning
